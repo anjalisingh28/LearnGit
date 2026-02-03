b6b486c2a7ff17ab87ad1250622cc8b5251f80f2
AdarshMalik21 <adarshmalik2001@gmail.com>
2026-02-03 16:17:21 +0530

    add component card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const user = [
+    {id: 1,
+      name: 'John Doe',
+    age: 30,
+    location: 'New York'},
+    {id: 2,
+      name: 'Jane Smith',
+    age: 25,
+    location: 'San Francisco'}
+  ]
   return (
     <>
       <div>Hello World</div>
+      {
+        user.map((item) =>(<Card key = {item.id} {...item}/>))
+      }
     </>
   )
 }
